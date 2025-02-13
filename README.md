e08eb485d0c89a04aa7d89be5ce2cf5f77e6fcef
Noah-Goodman229 <26goodmnoa@washk12.org>
2025-02-13 11:16:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# you-thought-this-was-a-repo-but-it-was-i-Dio
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Noah-Goodman229/you-thought-this-was-a-repo-but-it-was-i-Dio)
---
+++ index.js
@@ -0,0 +1,5 @@
+// run `node index.js` in the terminal
+
+console.log("hello world");//basic message
+console.log("guilty gear is the most well designed fighting game in the world");
+console.log("the balance of system and individual mechanics alone is masterful")
