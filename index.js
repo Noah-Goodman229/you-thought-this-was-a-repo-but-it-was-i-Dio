b266407e26cc5915049fead835ed07f226246c1c
Noah-Goodman229 <26goodmnoa@washk12.org>
2025-02-27 11:00:16 -0700

    card are complete

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // run `node index.js` in the terminal
 
-console.log("hello world");//basic message
-console.log("guilty gear is the most well designed fighting game in the world");
-console.log("the balance of system and individual mechanics alone is masterful")
+const prompt = require('prompt-sync')();
+
+// Get user input for card suit and rank
+let rank = prompt("think of a rank of card... ");
+let suit = prompt("now think of the suit for that card... ");
+
+// Combine the inputs into a greeting
+let cardpluck = "(draws card) your card is... the " + rank + " of " + suit + "! did i get it right?";
+
+console.log(cardpluck);
